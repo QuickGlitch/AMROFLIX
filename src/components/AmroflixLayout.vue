cbd995b8f84312b877d5babb11326b5f7a7d80f2
AdB <adb@AdBs-MacBook-Pro.local>
2026-02-07 10:57:20 +0100

    feat: setup sass preprocessing

--- src/components/AmroflixLayout.vue
+++ src/components/AmroflixLayout.vue
@@ -8,9 +8,20 @@ const { theme, toggleTheme } = useTheme()
 <template>
   <div class="amroflix-layout" :data-theme="theme">
     <header class="amroflix-layout__header">
-      <slot name="header"
-        ><AmroflixButton @click="toggleTheme">{{ theme }}</AmroflixButton></slot
-      >
+      <slot name="header">
+        <slot name="header__left"
+          ><div class="amroflix-layout__header__left">
+            <h1>Amroflix</h1>
+          </div></slot
+        >
+        <slot name="header__center"
+          ><div class="amroflix-layout__header__center">
+            <input type="text" placeholder="Search..." /></div
+        ></slot>
+        <slot name="header__right">
+          <AmroflixButton @click="toggleTheme">{{ theme }}</AmroflixButton>
+        </slot>
+      </slot>
     </header>
 
     <main class="amroflix-layout__body">
@@ -23,7 +34,7 @@ const { theme, toggleTheme } = useTheme()
   </div>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .amroflix-layout {
   /* component tokens */
   --amroflix-layout-header-text-color: var(--theme-text-default-color);
@@ -34,25 +45,43 @@ const { theme, toggleTheme } = useTheme()
   display: flex;
   flex-direction: column;
   min-height: 100vh;
-}
 
-.amroflix-layout__header {
-  display: flex;
-  justify-content: end;
-  background-color: var(--amroflix-layout-body-background-color);
-  color: var(--amroflix-layout-header-text-color);
-  padding: 1rem 2rem;
-}
+  &__header {
+    display: flex;
+    justify-content: end;
+    background-color: var(--amroflix-layout-body-background-color);
+    color: var(--amroflix-layout-header-text-color);
+    padding: 1rem 2rem;
 
-.amroflix-layout__body {
-  background-color: var(--amroflix-layout-body-background-color);
-  flex: 1;
-}
+    &__left {
+      flex-shrink: 1;
+      display: flex;
+      justify-content: start;
+    }
 
-.amroflix-layout__footer {
-  background-color: var(--amroflix-layout-footer-background-color);
-  color: var(--amroflix-layout-footer-text-color);
-  padding: 1rem 2rem;
-  text-align: center;
+    &__center {
+      flex-grow: 1;
+      display: flex;
+      justify-content: center;
+    }
+
+    &__right {
+      flex-shrink: 1;
+      display: flex;
+      justify-content: end;
+    }
+  }
+
+  &__body {
+    background-color: var(--amroflix-layout-body-background-color);
+    flex: 1;
+  }
+
+  &__footer {
+    background-color: var(--amroflix-layout-footer-background-color);
+    color: var(--amroflix-layout-footer-text-color);
+    padding: 1rem 2rem;
+    text-align: center;
+  }
 }
 </style>
